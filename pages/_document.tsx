7f9c6f6784a9e63342a1e4e79cbbf502abcc9cbd
acatzk <joshuaimalay@gmail.com>
2021-08-07 11:10:34 +0800

    🎉 init

---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+
+class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default Document
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NextPage } from 'next'
+
+const Index: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold text-purple-700 text-center">
+        Hello World
+      </h1>
+    </div>
+  )
+}
+
+export default Index
